1b0b9b424ac47b1240883b65ac714049bfdceb39
Osminalx <verdestro64@gmail.com>
2025-05-19 21:12:26 -0700

    :sparkles: AI integrations

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -3,20 +3,23 @@ return {
   version = "^5", -- Recommended
   lazy = false, -- This plugin is already lazy
   ft = "rust",
-  config = function()
-    local mason_registry = require("mason-registry")
-    local codelldb = mason_registry.get_package("codelldb")
-    local extension_path = codelldb:get_install_path() .. "/extension/"
-    local codelldb_path = extension_path .. "adapter/codelldb"
-    local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-    -- If you are on Linux, replace the line above with the line below:
-    -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-    local cfg = require("rustaceanvim.config")
+config = function()
+  local mason_registry = require("mason-registry")
+  if not mason_registry.has_package("codelldb") then
+    vim.notify("codelldb is not installed!", vim.log.levels.WARN)
+    return
+  end
 
-    vim.g.rustaceanvim = {
-      dap = {
-        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-      },
-    }
-  end,
+  local codelldb = mason_registry.get_package("codelldb")
+  local extension_path = codelldb:get_install_path() .. "/extension/"
+  local codelldb_path = extension_path .. "adapter/codelldb"
+  local liblldb_path = extension_path .. "lldb/lib/liblldb.so" -- Linux
+
+  local cfg = require("rustaceanvim.config")
+  vim.g.rustaceanvim = {
+    dap = {
+      adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+    },
+  }
+end,
 }
